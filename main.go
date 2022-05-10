4ebfed921f53074a3082c9610cfa1c763e713e34
compico <compico@mail.ru>
2022-05-10 16:02:57 +0300

    rename package

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 
 	"github.com/compico/osutools/filehelper"
+	"github.com/compico/osutools/internal/webserver"
+	"github.com/julienschmidt/httprouter"
 )
 
 var fh filehelper.OsuFolder
@@ -15,5 +17,10 @@ func main() {
 	err := fh.ReadOsudbFile()
 	if err != nil {
 		panic(err)
+	}
+	r := httprouter.New()
+	server := webserver.NewServer(":8000", r)
+	if err := server.S.ListenAndServe(); err != nil {
+		log.Fatalln(err)
 	}
 }
