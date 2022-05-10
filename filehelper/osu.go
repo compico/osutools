568bd924e12d3668cb18fd17735c9ae56db58f98
Maxim Lebedev <git@toby3d.me>
2022-05-10 16:48:01 +0500

    :triangular_flag_on_post: Used build tags for platform-specific OsuFolder method

--- filehelper/osu.go
+++ filehelper/osu.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/compico/osutools/encoding/database"
 	"github.com/compico/osutools/osu"
-	"golang.org/x/sys/windows/registry"
 )
 
 var unknownpath = errors.New("Unknown game path.")
@@ -32,25 +31,6 @@ func (osufolder *OsuFolder) initSongsPath() {
 
 func (osufolder *OsuFolder) initSkinsPath() {
 	osufolder.SkinsPath = filepath.Join(osufolder.GamePath, "Skins")
-}
-
-//For windows only
-func (osufolder *OsuFolder) InitGamePathByReg() error {
-	k, err := registry.OpenKey(registry.CLASSES_ROOT, `osu\DefaultIcon`, registry.QUERY_VALUE)
-	if err != nil {
-		return err
-	}
-	defer k.Close()
-
-	path, _, err := k.GetStringValue("")
-	if err != nil {
-		return err
-	}
-	path = path[1:]
-	path = filepath.Dir(path)
-
-	osufolder.GamePath = path
-	return nil
 }
 
 func (osufolder *OsuFolder) ReadOsudbFile() error {
---
+++ filehelper/osu_linix.go
@@ -0,0 +1,8 @@
+//go:build darwin || freebsd || linux || netbsd || openbsd
+
+package filehelper
+
+// TODO(toby3d)
+func (osufolder *OsuFolder) InitGamePathByReg() error {
+	return nil
+}
